ea3d7d4e0f4cacb9ebdac3a01e9b8e6726c4e803
tayyab212 <tthunjra@gmail.com>
2020-02-25 17:13:01 +0500

    reactive form approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create/post-create.component';
-import {  FormsModule } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule,MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressBarModule, MatProgressSpinnerModule} from '@angular/material'
 import {MatInputModule} from '@angular/material/input';
 import { HeaderComponent } from './header/header/header.component';
@@ -22,7 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../../post.model';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PostsService } from '../../post.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
@@ -16,16 +16,28 @@ export class PostCreateComponent implements OnInit {
   private mode = 'create'
   private postId: string;
   public post: Post;
-  isloading =false;
-  constructor(private postService: PostsService, private route: ActivatedRoute) { }
+  isloading = false;
+  postForm: FormGroup;
+  constructor(private postService: PostsService, private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.postForm = this.fb.group({
+      'title': ["", [Validators.required, Validators.minLength(3)]],
+      'content': ["", [Validators.required]],
+      'image':["",[Validators.required]]
+    });
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit'
         this.postId = paramMap.get('postId');
         this.postService.getPost(this.postId).subscribe(postData => {
           this.post = { id: postData._id, title: postData.title, content: postData.content };
+          this.postForm.setValue({
+           title:this.post.title,
+           content:this.post.content 
+          })
         });
 
       } else {
@@ -33,28 +45,44 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     })
+
   }
 
-  onAddPost(form: NgForm) {
+  onAddPost() {
     debugger;
-    if (form.invalid) {
+    if (this.postForm.invalid) {
       return;
     }
     const post: Post = {
       id: null,
-      title: form.value.title,
-      content: form.value.content
+      title: this.postForm.value.title,
+      content: this.postForm.value.content,
+    
     }
     this.isloading = true;
     if (this.mode == 'create') {
-      this.postService.addPost(form.value.title, form.value.content)
+      this.postService.addPost(this.postForm.value.title, this.postForm.value.content)
     } else {
-      this.postService.updatePost(this.postId, form.value.title, form.value.content)
+      this.postService.updatePost(this.postId, this.postForm.value.title, this.postForm.value.content)
     }
-
-    form.reset();
+    this.postForm.reset();
   }
 
+  get title() { return this.postForm.get('title'); }
 
+  get content() {return this.postForm.get('content');}
+  url: any;
+  onSelectFile(event) {
+    debugger;                // called each time file input changes
+      if (event.target.files && event.target.files[0]) {
+        var reader = new FileReader();
+  
+        reader.readAsDataURL(event.target.files[0]); // read file as data url
+  
+        reader.onload = (event) => { // called once readAsDataURL is completed
+          this.url = event.target.result;
+        }
+      }
+  }
 
 }
